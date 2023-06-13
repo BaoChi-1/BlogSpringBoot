cde7448a0248be58bfc1823934b3ee377e79b95f
Никита Кремнёв <nkremnev3003@gmail.com>
2023-06-13 13:25:04 +0300

    Реализовал добавление статей в БД через интерфейс сайта.

--- src/main/java/com/first/blog/controllers/BlogController.java
+++ src/main/java/com/first/blog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import com.first.blog.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -24,5 +26,12 @@ public class BlogController {
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
         return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect :/blog";
     }
 }
